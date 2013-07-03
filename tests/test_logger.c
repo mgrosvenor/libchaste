d96fa0e4a473ac0039683076e0c90c7761c2a513
Matthew P. Grosvenor <mgrosvenor@gmail.com>
2013-07-03 16:28:51 +0200

    MPG: Cleaned up the test_logger a little to make it a bit more senseible

--- tests/test_logger.c
+++ tests/test_logger.c
@@ -5,27 +5,34 @@
  *      Author: mgrosvenor
  */
 
-//Customize the logging build level
+//Customize the logging build level - This is not necessary and can be omitted if you are happy with the default
+//This MUST come before #include "../log/log.h"!
+//**************************************************************************************************************
 #include "../log/log_levels.h"
 #define M6_LOG_BUILD_LVL M6_LOG_LVL_DEBUG1
+//**************************************************************************************************************
+
 
 //Pull in the logger
 #include "../log/log.h"
 
 //Use it
-USE_M6_LOGGER(true,m6_log_tostderr,"out.log");
+USE_M6_LOGGER(M6_LOG_LVL_INFO,true,m6_log_tostdout,"out.log");
 
 int main(int argc, char** argv)
 {
     int i = 0;
-    for(; i < 10024; i++){ //Loop around a few times. Use valgrind to make sure that there are no memory leaks
-        //m6_log_fatal("blah blah%u\n", 43);
-        m6_log_error("blah blah%u\n", 42);
-        m6_log_warn("blah blah%u\n", 44);
-        m6_log_info("blah blah%u\n", 42);
-        m6_log_debug1("blah blah%u\n", 42);
-        m6_log_debug2("blah blah%u\n", 47);
-        m6_log_debug3("blah blah%u\n", 42);
+    for(; i < 1024; i++){ //Loop around a few times. Use valgrind to make sure that there are no memory leaks
+
+        m6_log_error("Error text here - %u\n", i);
+        m6_log_warn("Warning text here - %u\n", i);
+        m6_log_info("Info text here - %u\n", i);
+        m6_log_debug1("Debug text here - %u\n", i);
+        m6_log_debug2("Debug 2 text here - %u\n", i);
+        m6_log_debug3("Debug 3 text here - %u\n", i);
     }
+
+    m6_log_fatal("Fatal error text here %u\n", i);
+
     return 0;
 }
