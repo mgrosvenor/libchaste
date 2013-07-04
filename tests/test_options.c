047806848c2310ef60a46e58cb8d145dc5eb6983
Matthew P. Grosvenor <mgrosvenor@gmail.com>
2013-07-04 15:55:06 +0200

    MPG added testing for optional arguments

--- tests/test_options.c
+++ tests/test_options.c
@@ -19,12 +19,14 @@ struct {
 
 int main(int argc, char** argv)
 {
-    m6_opt_addu(M6_OPTION_REQUIRED,'a',"first","This is the 1st option", &options.opt1, 43);
-    m6_opt_addi(M6_OPTION_REQUIRED,'b',"second","This is the 2nd option", &options.opt2, -43);
-    m6_opt_addb(M6_OPTION_REQUIRED,'c',"third","This is the 3rd option", &options.opt3, true);
-    m6_opt_adds(M6_OPTION_REQUIRED,'d',"fourth","This is the 4th option", &options.opt4, "ha ha");
-    m6_opt_addf(M6_OPTION_REQUIRED,'e',"fifth","This is the 5th option", &options.opt5, 43.0);
+    m6_opt_addu(M6_OPTION_OPTIONAL,'a',"first","This is the 1st option", &options.opt1, 43);
+    m6_opt_addi(M6_OPTION_OPTIONAL,'b',"second","This is the 2nd option", &options.opt2, -43);
+    m6_opt_addb(M6_OPTION_OPTIONAL,'c',"third","This is the 3rd option", &options.opt3, true);
+    m6_opt_adds(M6_OPTION_OPTIONAL,'d',"fourth","This is the 4th option", &options.opt4, "ha ha");
+    m6_opt_addf(M6_OPTION_OPTIONAL,'e',"fifth","This is the 5th option", &options.opt5, 43.0);
     m6_opt_parse(argc,argv);
+
+    printf("a=%lu, b=%li, c=%i, d=%s, e=%lf\n", options.opt1, options.opt2, options.opt3, options.opt4, options.opt5);
 
 
     return 0;
