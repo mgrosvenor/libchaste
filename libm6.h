7ff3acb139ec1313a5b6b524edf2ba895c93b938
Matthew P. Grosvenor <mgrosvenor@gmail.com>
2013-07-08 00:55:35 +0200

    MPG: Added all the working headers to libm6.h, now ready to rock

--- libm6.h
+++ libm6.h
@@ -8,6 +8,11 @@
 #ifndef LIBM6_H_
 #define LIBM6_H_
 
-
+#include "options/m6_options.h"
+#include "perf/m6_perf.h"
+#include "log/log.h"
+#include "deps/libbstring/bstraux.h"
+#include "deps/libbstring/bstrlib.h"
+#include "deps/libcgen/vector.h"
 
 #endif /* LIBM6_H_ */
