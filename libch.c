9ba4782c11ff03318534594b18eb0255eb7fc87c
= <=>
2013-09-11 15:30:28 +0100

    Improved the build to now buid against libch.c and run unit tests

---
+++ libch.c
@@ -0,0 +1,16 @@
+/*
+ * libch.c
+ *
+ *  Created on: Sep 11, 2013
+ *      Author: mgrosvenor
+ */
+
+
+#include "libch.h"
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+    return 0;
+}
