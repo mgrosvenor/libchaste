cc5602c7fbaa2ef9263432b27d3eda10bfafc218
Matthew P. Grosvenor <mgrosvenor@gmail.com>
2013-09-19 10:42:18 +0100

    Made libch.c ready for libridisation

--- libch.c
+++ libch.c
@@ -5,16 +5,6 @@
  *      Author: mgrosvenor
  */
 
-
 #include "libch.h"
-#include "log/log.h"
 
-USE_CH_LOGGER_DEFAULT;
-USE_CH_OPTIONS;
 
-int main(int argc, char** argv)
-{
-    (void)argc;
-    (void)argv;
-    return 0;
-}
