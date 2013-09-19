0a44e0500f658c34791c1df4992471fa84d78cdd
Matthew P. Grosvenor <mgrosvenor@gmail.com>
2013-09-19 11:58:53 +0100

    Fixed build_release.sh to include README and LICENSE files

--- build_release.sh
+++ build_release.sh
@@ -1,28 +1,37 @@
 #! /bin/sh
-
+##################################
+#Build the source in release mode#
+##################################
 
 ./build.sh --variant=release
 
-#Now make the release tarball
+##################################
+#Now make the release tarball    #
+##################################
 
+#Set up a working directory
 tmp="bin/RELEASETAR/libchaste"
-
 mkdir -p $tmp
 
+#Build up an include directory in it
 ./makeinclude.sh $tmp > $tmp/.tmp.makeincludeout
 rm -rf $tmp/.tmp.makeincludeout
 
+#Copy specific files into place
 cp bin/libchaste.a $tmp
+cp LICENSE $tmp
+cp README $tmp
+
+#Make the tarball
 cd $tmp/../
 tar -cjf libchaste.tar.bz2 libchaste
-
 cd -
 
+#Copy out the tarball 
 cp $tmp/../libchaste.tar.bz2 .
 
 #Clean up the mess
 rm -rf $tmp
-
 
 
 
