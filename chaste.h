e3dbe788c6befd50276688911e676be0ed762bac
Matthew P. Grosvenor <mgrosvenor@gmail.com>
2013-09-20 11:29:27 +0100

    Renamed libchaste.h to chaste.h to be consistent

---
+++ chaste.h
@@ -0,0 +1,27 @@
+/*
+ * libm6.h
+ *
+ *  Created on: Jun 29, 2013
+ *      Author: mgrosvenor
+ */
+
+#ifndef LIBM6_H_
+#define LIBM6_H_
+
+#include "types/types.h"
+#include "utils/util.h"
+#include "string/string.h"
+
+#include "options/options.h"
+#include "perf/perf.h"
+#include "log/log.h"
+
+
+#include "data_structs/array/array_std.h"
+#include "data_structs/vector/vector_std.h"
+#include "data_structs/linked_list/linked_list.h"
+
+
+
+
+#endif /* LIBM6_H_ */
