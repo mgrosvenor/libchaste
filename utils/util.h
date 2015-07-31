6300e094a0ddfbfcb7d94a39e6493d4365603962
= <=>
2015-07-31 10:20:37 +0100

    Added a 'equally' likely case definition for cases where it's hard to tell what the correct branch is.

--- utils/util.h
+++ utils/util.h
@@ -10,6 +10,7 @@
 
 #define likely(x)       __builtin_expect((x),1)
 #define unlikely(x)     __builtin_expect((x),0)
+#define eqlikely(x)     (x)
 
 #define MIN(x,y) ( (x) < (y) ?  (x) : (y))
 #define MAX(x,y) ( (x) > (y) ?  (x) : (y))
