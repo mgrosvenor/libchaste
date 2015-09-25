28b98bf8e56c9d920fc44fc64d4403766dd00c12
= <=>
2015-09-25 12:40:36 +0100

    Added a catch to stop apple sys getting pulled in to linux compiles

--- sys/sys_apple.h
+++ sys/sys_apple.h
@@ -11,8 +11,11 @@
 #include <time.h>
 #include <sys/time.h>
 
+#ifdef __APPLE__ //These definitions are only required for apple/darwin
 
 #define CLOCK_REALTIME 0
 void clock_gettime(int ignored, struct timespec *ts);
+
+#endif
 
 #endif /* SYS_APPLE_H_ */
